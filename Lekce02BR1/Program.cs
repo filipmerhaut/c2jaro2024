63e8893729c61f72b93eb2e699b96ff1907736c6
filipmerhaut <filip.merhaut@gmail.com>
2024-05-05 22:43:19 +0200

    Update Program.cs
    Add basic logic

--- Lekce02BR1/Program.cs
+++ Lekce02BR1/Program.cs
@@ -5,11 +5,25 @@ namespace Lekce02BR1
     {
         static void Main(string[] args)
         {
+            int den;
+            int mesic;
+            int rok;
+            Console.WriteLine("Zadej den odjezdu na dovolenou: ");
+            den = int.Parse(Console.ReadLine());            
+            Console.WriteLine("Zadej měsíc odjezdu na dovolenou: ");
+            mesic = int.Parse(Console.ReadLine());
+            Console.WriteLine("Zadej rok odjezdu na dovolenou: ");
+            rok = int.Parse(Console.ReadLine());
+            DateTime datumOdjezdu = new DateTime(rok, mesic, rok);
+            TimeSpan ts = DateTime.Now - datumOdjezdu;
+            Console.WriteLine(ts.ToString());
+            Console.ReadLine();
+
             // Zbyvajici dny do dovolene
             // Postupne se uzivatele zeptej na den, mesic a rok odjezdu na dovolenou. Pomoci DateTime a TimeSpan vypocitej pocet
             // zbyvajicich dni do odjezdu na dovolenou a vypis je na konzoli.
             // Pozn.: pro zjednoduseni staci pouzit int.Parse
-            Console.WriteLine("Hello, World!");
+
         }
     }
 }
